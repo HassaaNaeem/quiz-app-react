c3af511b2caf01b88f235ca7f3e41d9c7d3a20ed
HassaaNaeem <hassaan282006@gmail.com>
2026-03-11 21:39:03 +0500

    Added CSS and Date Counter

---
+++ src/DateCounter.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+function DateCounter() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+
+  const date = new Date("March 11 2026");
+  date.setDate(date.getDate() + count);
+
+  const inc = () => {
+    setCount((count) => count + step);
+  };
+  const dec = () => {
+    setCount((count) => count - step);
+  };
+
+  const defineCount = (e) => {
+    setCount(Number(e.target.value));
+  };
+  const defineStep = (e) => {
+    setStep(Number(e.target.value));
+  };
+
+  const reset = () => {
+    setCount(0);
+    setStep(1);
+  };
+
+  return (
+    <div className="counter">
+      <div>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={defineStep}
+        />
+        <span>{step}</span>
+      </div>
+
+      <div>
+        <button onClick={dec}>-</button>
+        <input value={count} onChange={defineCount} />
+        <button onClick={inc}>+</button>
+      </div>
+
+      <p>{date.toDateString()}</p>
+
+      <div>
+        <button onClick={reset}>Reset</button>
+      </div>
+    </div>
+  );
+}
+
+export default DateCounter;
